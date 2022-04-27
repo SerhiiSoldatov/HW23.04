b7d2aea9bbfa842f49a3406dfa241007c6b24f7c
Serhii <serhii.soldatov@gmail.com>
2022-04-27 12:00:26 +0200

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,18 @@
 // степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+
+
+Console.Write("Введите число   : ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите степень : ");
+int b = int.Parse(Console.ReadLine());
+
+int x = 1;
+for (int i = 0; i < b; i++)
+{
+    x *= a;
+}
+Console.WriteLine($"{a} ^ {b} = {x}");
